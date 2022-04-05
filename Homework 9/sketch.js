a6fae011e49fff4b9b4cc03b68ee6013ed340e6b
Jack Spring Macbook Air <jackspring@Jacks-MacBook-Air-2.local>
2022-04-04 23:02:01 -0600

    Homework 11

--- Homework 9/sketch.js
+++ Homework 9/sketch.js
@@ -1,3 +1,22 @@
+var bodyX = 200;
+var bodyY = 185
+var bodyDirection =3;
+
+var headX =250;
+var headY=100;
+var headDirection =3;
+
+var size = 24;
+var count = 0;
+var sizeDirection = 3;
+
+var pointX = 220;
+var pointY = 75;
+var pointDirection = 4;
+
+var mouthX = 250;
+var mouthY = 100;
+var mouthDirection = 1;
 function setup()
 {
      createCanvas(500, 600);
@@ -8,25 +27,48 @@ function setup()
  
      background(120);
 
-textSize(20)
-fill('red')
+textSize(size);
+fill('red');
+size+= sizeDirection;
+count++;
+if(count > 5)
+{
+     sizeDirection *=-1;
+     count = 0;
+}
 text("Jack Spring", 10,80);
 
 // head
 fill('rgba(0,255,0, 0.25)');
-circle(250,75,125);
+circle(headX,headY,125);
+headX+=headDirection;
+if(headX >= 418 || headX <= 82)
+{
+     headDirection *= -1;
+}
 
 // eyes
 strokeWeight(10);
 fill(0);
-point(220,75);
+point(pointX,75);
+pointX+=pointDirection;
+if(pointX >= 418 || pointX <=82)
+{
+     pointDirection *= -1;
+}
 point(285,75);
 
 // nose
 point(245,90);
 
 // mouth
-ellipse(250, 100, 45, 30)
+ellipse(250, mouthY, 45, 30)
+mouthY += mouthDirection;
+if(mouthY <= 0 || mouthY >= 450)
+{
+     mouthDirection *= -1;
+}
+
 
 // hair
 fill(255, 204, 0)
@@ -35,7 +77,12 @@ line(250,10,360,100);
 line(250,10,280,100)
 // body
 fill('#222222');
-rect(200,185,100,150);
+rect(200,bodyY,100,150);
+bodyY += bodyDirection;
+if(bodyY <= 0 || bodyY >= 450)
+{
+     bodyDirection *= -1;
+}
 
 // decoration
 fill(255);
